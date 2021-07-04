b6740a55a980c795aff7e257a5bf921c2f69fb3a
lucaslsg-prog <lsg.comunicacoes@gmail.com>
2021-07-04 19:25:11 -0300

    Register new users by Email and Passowrd

--- App.js
+++ App.js
@@ -1,112 +1,77 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet,TextInput, LogBox, Button} from 'react-native';
+import firebase from './src/firebaseConnection';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+LogBox.ignoreAllLogs=true;
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App(){
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+  async function cadastrar(){
+    await firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((value) => {
+        alert('Usuário criado: ' + value.user.email);
+      })
+      .catch((error) => {
+        if(error.code === 'auth/weak-password'){
+          alert('Sua senha deve ter pelo menos 6 caracteres');
+          return;
+        }
+        if(error.code === 'auth/invalid-email'){
+          alert('Email invalido');
+          return;
+        }else{
+          alert('Ops algo deu errado.')
+          return;
+        }
+      })
+      setEmail('');
+      setPassword('');
+  }
+
+  return(
+    <View style={styles.container}>
+      <Text style={styles.texto}>Email</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid="transparent"
+        onChangeText={(texto)=>setEmail(texto)}
+        value={email}
+      />
+
+      <Text style={styles.texto}>Password</Text>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid="transparent"
+        onChangeText={(texto)=>setPassword(texto)}
+        value={password}
+      />
+
+      <Button
+        title="Cadastrar"
+        onPress={cadastrar}
+      />
     </View>
   );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container:{
+    flex:1,
+    margin:10,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  texto:{
+    fontSize:20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  input:{
+    marginBottom:10,
+    padding: 10,
+    borderWidth:1,
+    borderColor:'#121212',
+    height: 40,
+    fontSize:17
+  }
 });
 
-export default App;
